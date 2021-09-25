1fa28be1b96a62c3d5158c478b88231bd6bbe074
Ikedaaaa <matheus-ikeda@hotmail.com>
2021-09-25 17:28:54 -0300

    Utilizando Spring Data JPA e implementando métodos de pesquisa

--- MergulhoSpringRest-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ MergulhoSpringRest-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -2,22 +2,26 @@ package com.algaworks.algalog.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+	private ClienteRepository clienteRepository;
+
+	public ClienteController(ClienteRepository clienteRepository) {
+		super();
+		this.clienteRepository = clienteRepository;
+	}
 
 	@GetMapping("/clientes")
-	public List<Cliente> listar() {		
-		return manager.createQuery("from Cliente", Cliente.class).getResultList();
+	public List<Cliente> listar() {
+		//return clienteRepository.findByNome("Matheus Ikeda");
+		//return clienteRepository.findByNomeContaining("Matheus");
+		return clienteRepository.findAll();
 	}
 }
