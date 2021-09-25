9a03b8fcda2d468441e1395bb412ab313b0ea656
Ikedaaaa <matheus-ikeda@hotmail.com>
2021-09-25 16:57:54 -0300

    Primeira consulta ao banco de dados utilizando JPA

--- MergulhoSpringRest-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ MergulhoSpringRest-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,21 +12,12 @@ import com.algaworks.algalog.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
+	
+	@PersistenceContext
+	private EntityManager manager;
 
 	@GetMapping("/clientes")
-	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Matheus Ikeda");
-		cliente1.setTelefone("11 4536-3847");
-		cliente1.setEmail("abc@gmail.com");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Matheus Marques");
-		cliente2.setTelefone("95 4256-5467");
-		cliente2.setEmail("123@gmail.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+	public List<Cliente> listar() {		
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }
