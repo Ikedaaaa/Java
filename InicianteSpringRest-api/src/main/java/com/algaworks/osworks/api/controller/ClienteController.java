174aa46e7bf68f0960cc72fb7ac7dbbd04b0bf8b
Ikedaaaa <matheus-ikeda@hotmail.com>
2021-10-14 19:37:10 -0300

    Criado Repositório de Clientes e implementada listagem de todos os clientes, listagem por nome e listagem com "like" de nome

--- InicianteSpringRest-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ InicianteSpringRest-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
@@ -16,8 +19,21 @@ public class ClienteController {
 	@PersistenceContext
 	private EntityManager manager;
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class).getResultList();
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/pesquisanome")
+	public List<Cliente> listarPorNome(@RequestParam String nome) {
+		return clienteRepository.findByNome(nome);
+	}
+	
+	@GetMapping("/clientes/pesquisacontaining")
+	public List<Cliente> listarPorNomeContaining(@RequestParam String nomelike) {
+		return clienteRepository.findByNomeContaining(nomelike);
 	}
 }
