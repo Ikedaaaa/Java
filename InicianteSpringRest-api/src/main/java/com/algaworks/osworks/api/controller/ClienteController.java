7e687879959d4d630da1bed4d1c58628f705e73c
Ikedaaaa <matheus-ikeda@hotmail.com>
2021-10-30 13:43:07 -0300

    Implementado o CRUD de cliente

--- InicianteSpringRest-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ InicianteSpringRest-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,19 +1,30 @@
 package com.algaworks.osworks.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
 import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
 	@PersistenceContext
@@ -22,18 +33,58 @@ public class ClienteController {
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
 	}
 	
-	@GetMapping("/clientes/pesquisanome")
+	@GetMapping("/pesquisanome")
 	public List<Cliente> listarPorNome(@RequestParam String nome) {
 		return clienteRepository.findByNome(nome);
 	}
 	
-	@GetMapping("/clientes/pesquisacontaining")
+	@GetMapping("/pesquisacontaining")
 	public List<Cliente> listarPorNomeContaining(@RequestParam String nomelike) {
 		return clienteRepository.findByNomeContaining(nomelike);
+	}
+	
+	@GetMapping("/{idcliente}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long idcliente) {
+		Optional<Cliente> cliente = clienteRepository.findById(idcliente);
+		
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{idcliente}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long idcliente, @RequestBody Cliente cliente){
+		if (!clienteRepository.existsById(idcliente)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setId(idcliente);
+		cliente = clienteRepository.save(cliente);
+		
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{idcliente}")
+	public ResponseEntity<Void> remover(@PathVariable Long idcliente){
+		if (!clienteRepository.existsById(idcliente)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clienteRepository.deleteById(idcliente);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
