72c6c5a335c53ac3de30346abf85e2e23e45ee2c
Ikedaaaa <matheus-ikeda@hotmail.com>
2021-10-31 13:20:38 -0300

    LocalDateTime -> OffsetDateTime
    Alteração de LocalDateTime para OffsetDateTime - boa prática para trabalhar com data e hora de acordo com o UTC - ISO-8601

--- InicianteSpringRest-api/src/main/java/com/algaworks/osworks/domain/model/OrdemServico.java
+++ InicianteSpringRest-api/src/main/java/com/algaworks/osworks/domain/model/OrdemServico.java
@@ -1,7 +1,7 @@
 package com.algaworks.osworks.domain.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -51,11 +51,11 @@ public class OrdemServico {
 	
 	@JsonProperty(access = Access.READ_ONLY)
 	@Column(name="data_abertura")
-	private LocalDateTime dataAbertura;
+	private OffsetDateTime dataAbertura;
 	
 	@JsonProperty(access = Access.READ_ONLY)
 	@Column(name="data_finalizacao")
-	private LocalDateTime dataFinalizacao;
+	private OffsetDateTime dataFinalizacao;
 	
 	public Long getId() {
 		return id;
@@ -87,16 +87,16 @@ public class OrdemServico {
 	public void setStatus(StatusOrdemServico status) {
 		this.status = status;
 	}
-	public LocalDateTime getDataAbertura() {
+	public OffsetDateTime getDataAbertura() {
 		return dataAbertura;
 	}
-	public void setDataAbertura(LocalDateTime dataAbertura) {
+	public void setDataAbertura(OffsetDateTime dataAbertura) {
 		this.dataAbertura = dataAbertura;
 	}
-	public LocalDateTime getDataFinalizacao() {
+	public OffsetDateTime getDataFinalizacao() {
 		return dataFinalizacao;
 	}
-	public void setDataFinalizacao(LocalDateTime dataFinalizacao) {
+	public void setDataFinalizacao(OffsetDateTime dataFinalizacao) {
 		this.dataFinalizacao = dataFinalizacao;
 	}
 	
