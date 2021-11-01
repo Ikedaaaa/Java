e372e039a18c67f8a4e245d16c82b39ddde906a6
Ikedaaaa <matheus-ikeda@hotmail.com>
2021-11-01 01:34:30 -0300

    Criação do Projeto
    Criação do Projeto Regesc desenvolvido com prof samuka - xavecoding

---
+++ SpringData-Xavecoding/src/main/java/br/com/xavecoding/regesc/SpringDataXavecodingApplication.java
@@ -0,0 +1,13 @@
+package br.com.xavecoding.regesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataXavecodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataXavecodingApplication.class, args);
+	}
+
+}
