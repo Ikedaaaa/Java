ffa4dc967c104c60d49b239cfdbb352014db3f3b
Ikedaaaa <matheus-ikeda@hotmail.com>
2021-11-01 02:23:45 -0300

    Criação do Repositório de Professor
    Criação do Repositório de Professor e teste básico para ver a persistência de dados pelo console usando o System.out.println
    
    Aula 5
    Projeto Regesc desenvolvido com prof samuka - xavecoding

--- SpringData-Xavecoding/src/main/java/br/com/xavecoding/regesc/SpringDataXavecodingApplication.java
+++ SpringData-Xavecoding/src/main/java/br/com/xavecoding/regesc/SpringDataXavecodingApplication.java
@@ -1,13 +1,37 @@
 package br.com.xavecoding.regesc;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.xavecoding.regesc.orm.Professor;
+import br.com.xavecoding.regesc.repository.ProfessorRepository;
+
 @SpringBootApplication
-public class SpringDataXavecodingApplication {
+public class SpringDataXavecodingApplication implements CommandLineRunner {
+
+	private ProfessorRepository repository;
+	
+	public SpringDataXavecodingApplication(ProfessorRepository repository) {
+		super();
+		this.repository = repository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataXavecodingApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Professor professor = new Professor("Qualquer coisa", "sp1751837");
+		
+		System.out.println("Professor ANTES do save (persistência com BD)");
+		System.out.println(professor);
+		
+		this.repository.save(professor);
+		
+		System.out.println("Professor DEPOIS do save (persistência com BD)");
+		System.out.println(professor);
 	}
 
 }
