11065d116b386e6cb957ca2f51297570bbbe11a1
Ikedaaaa <matheus-ikeda@hotmail.com>
2021-11-06 14:04:55 -0300

    Introdução ao Thymeleaf
    Manipulação de View com thymeleaf;
    
    Renderizando a view de diferentes formas:
    HttpServletRequest;
    Model;
    ModelAndView.
    
    Aula 4
    Projeto RegescWeb desenvolvido com prof samuka - xavecoding

--- SpringMVC-Xavecoding/src/main/java/br/com/xavecoding/regescweb/controllers/HelloController.java
+++ SpringMVC-Xavecoding/src/main/java/br/com/xavecoding/regescweb/controllers/HelloController.java
@@ -1,14 +1,38 @@
 package br.com.xavecoding.regescweb.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
 	@GetMapping("/hello")
-	public String hello() {
-		return "hello"; //Spring vai renderizar o arquivo templates/hello.html
+	public String hello(String arquivo) {
+		arquivo = "hello";
+		return arquivo; //Spring vai renderizar o arquivo templates/hello.html
+	}
+	
+	@GetMapping("/hello-servlet")
+	public String hello(HttpServletRequest request) {
+		request.setAttribute("nome", "Berlin");
+		return "hello";
+	}
+	
+	@GetMapping("/hello-model")
+	public String hello(Model model) {
+		model.addAttribute("nome", "Rio");
+		return "hello";
+	}
+	
+	@GetMapping("/hello-mv")
+	public ModelAndView hello() {
+		ModelAndView mv = new ModelAndView("hello"); // nome do arquivo html a ser renderizado/exibido
+		mv.addObject("nome", "Moscow");
+		return mv;
 	}
 	
 }
