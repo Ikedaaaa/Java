a5835a2597d46219e503457a0aa01dd2e36de59a
Ikedaaaa <matheus-ikeda@hotmail.com>
2021-11-22 17:24:47 -0300

    Página de Detalhes de Professor
    Criada página html para mostrar detalhes do professor com thymeleaf;
    Utilização de th:href para definir o link da página ao clicar em detalhes na Lista de Professores
    
    Aula 18
    Projeto RegescWeb desenvolvido com prof samuka - xavecoding

--- SpringMVC-Xavecoding/src/main/java/br/com/xavecoding/regescweb/controllers/ProfessorController.java
+++ SpringMVC-Xavecoding/src/main/java/br/com/xavecoding/regescweb/controllers/ProfessorController.java
@@ -1,12 +1,14 @@
 package br.com.xavecoding.regescweb.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -52,6 +54,22 @@ public class ProfessorController {
 		} else {
 			Professor professor = novoProfessor.toProfessor();
 			this.professorRepository.save(professor);
+			return new ModelAndView("redirect:/professores/" + professor.getId());
+		}
+	}
+	
+	@GetMapping("professores/{id}")
+	public ModelAndView show(@PathVariable Long id) {
+		Optional<Professor> optional = this.professorRepository.findById(id);
+		
+		if(optional.isPresent()) {
+			Professor professor = optional.get();
+			
+			ModelAndView mv = new ModelAndView("professores/show");
+			mv.addObject("professor", professor);
+			
+			return mv;
+		} else {
 			return new ModelAndView("redirect:/professores");
 		}
 	}
