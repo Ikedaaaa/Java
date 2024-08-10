b3c5cf135afff2176f74a79c97566c3046f8a696
Ikedaaaa <ikedaaaa@users.noreply.github.com>
2024-08-10 14:38:09 -0300

    ILP007 - POO: Prova de Proficiência (Questão 1)
    Faça um programa em Java que leia e armazene o salário de uma amostra de 15 trabalhadores. Calcule a média salarial e indique a estatística de: quantas pessoas estão abaixo dessa média, quantas estão acima e quantas estão na média

---
+++ ILP007-ProgramacaoOrientadaAObjetos_ProvaProficiencia/Questao1/src/br/gov/sp/fatec/poo/provaproficiencia/Questao1.java
@@ -0,0 +1,40 @@
+package br.gov.sp.fatec.poo.provaproficiencia;
+import java.util.Scanner;
+
+public class Questao1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		float[] salarios = new float[15];
+		float media = 0;
+		
+		int abaixoMedia = 0;
+		int naMedia = 0;
+		int acimaMedia = 0;
+		
+		System.out.println("Digite o salário de 15 trabalhadores:");
+		for (int i = 0; i < 15; i++) {
+			System.out.print((i+1)+"º trabalhador: R$");
+			salarios[i] = scanner.nextFloat();
+			media += salarios[i];
+		}
+		scanner.close();
+		media = media / 15;
+		
+		for (float salario: salarios) {
+			if (salario == media)
+				naMedia++;
+			else if (salario < media)
+				abaixoMedia++;
+			else
+				acimaMedia++;
+		}
+		
+		System.out.println("\n******** ESTATÍSTICAS ********\n");
+		System.out.println("Pessoas abaixo da média: " + abaixoMedia);
+		System.out.println("Pessoas acima da média: " + acimaMedia);
+		System.out.println("Pessoas na média: " + naMedia);
+	}
+
+}
